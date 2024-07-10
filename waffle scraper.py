d18adaa8b107858888e3176965556cf4c91f637b
jake-learns <jacob.d.campbell92@gmail.com>
2024-07-10 16:17:16 -0500

    Reorganized code into functions, added some boilerplate code, added starter docstrings.

--- waffle scraper.py
+++ waffle scraper.py
@@ -3,80 +3,101 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-# #options for headless mode
-# options = ChromeOptions()
-# options.add_argument("--headless=new")
-# driver = webdriver.Chrome(options=options)
 
-#option for regular web browser
-driver = webdriver.Chrome()
+def main():
+    #options for headless mode
+    options = ChromeOptions()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(options=options)
 
-driver.get("https://wafflegame.net/archive")
+    driver.get("https://wafflegame.net/archive")
+    driver.implicitly_wait(3)
+    #waits until the data is displayed and visible on the screen
+    items = WebDriverWait(driver, 10).until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'stat-row__val')))
 
-#waits until the data is displayed and visible on the screen
-WebDriverWait(driver, 5).until(EC.visibility_of_any_elements_located((By.CLASS_NAME, 'stat-row__val')))
-items = driver.find_elements(By.CLASS_NAME, 'stat-row__val')
-for item in items:
-    #there are a few blanks here, data that isn't actually visible in the current vew and thus empty
-    #so I just am looking to grab the first non-blank element and strip out just the max puzzle count and stores it
-    if item.text != "":
-        #strips the txt from a value of '0 / 899 (0%)' ->  '899'
-        num_puzzles = int(item.text.rsplit("/ ")[1].rsplit(" (")[0])
-        break
+    for item in items:
+        #there are a few blanks here, data that isn't actually visible in the current vew and thus empty
+        #so I just am looking to grab the first non-blank element and strip out just the max puzzle count and stores it
+        if item.text != "":
+            #strips the txt from a value of '0 / 899 (0%)' ->  '899'
+            num_puzzles = int(item.text.rsplit("/ ")[1].rsplit(" (")[0])
+            break
 
-for i in range(1, num_puzzles + 1):
-    # driver.get("https://wafflegame.net/archive")
-    # wait for the item with the specific data-id to be present
-    item = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//*[@data-id='{i}']")))
-    # scroll the element into view
-    driver.execute_script("arguments[0].scrollIntoView();", item)
-    
-    # wait until the element is clickable and then click it
-    # time.sleep(.1)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//*[@data-id='{i}']"))).click()
+    for i in range(1, num_puzzles + 1):
+        scroll_to_next_puzzle(driver, i)
+        tile_data_list = scrape_tiles(driver)
+        for tile_data in tile_data_list:
+            print(tile_data)
+        print(f"Puzzle {i} complete.")
+        #clicks the back button to return to the archive menu
+        driver.find_element(By.CSS_SELECTOR, "button.button--back.icon-button").click()
 
-    # time.sleep(.1)
-    # Find all tile elements
-    wait = WebDriverWait(driver, 20)
-    
-    # find elements with class names starting with "tile draggable tile" - notated by the ^ 
-    tiles = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[class^='tile draggable tile']")))
+def tile_swap(actions, driver, source_pos, target_pos):
+    """
+    Pre-Condition: Known tiles to be swapped will be provided, order is irrelevant
+    Post-Condition: Tiles will be swapped on screen
+    ---------------
+    This function is provided the "data-pos" value of two tiles (as well as the WebDriver and ActionChain objects)
+    and simply swaps their locations on screen. 
+    """
+    source_tile_pos = driver.find_element(By.CSS_SELECTOR, f"[data-pos='{source_pos}']")
+    target_tile_pos = driver.find_element(By.CSS_SELECTOR, f"[data-pos='{target_pos}']")
+    actions.drag_and_drop(source_tile_pos, target_tile_pos).perform()
 
-    tile_data = []
+def scrape_tiles(driver):
+    """
+    Pre-Condition: Puzzle to be scraped will be loaded on screen
+    Post-Condition: Data for all the current letter tiles will be returned
+    ---------------
+    This function will grab all the elements on screen that correlate with the letter tiles that are present on the current board.
+    It will grab some data from each tile: the letter in plain text between the <div></div>, the color from the class name, and
+    the position as notated by an attribute called "data-pos". Then the data for the board is returned. 
+    """
+    tiles = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "[class^='tile draggable tile']")))
     count = 0
+    tile_data = []
+
     for tile in tiles:
         letter = tile.text
 
         #DEBUG
         if letter == "":
             quit()
-        
+
+        position = tile.get_attribute('data-pos')
         classes = tile.get_attribute('class').split()
         color = None
+
         if 'green' in classes:
             color = 'green'
         elif 'yellow' in classes:
             color = 'yellow'
-        # elif 'gray' in classes:
-        #     color = 'gray'
-        # elif 'fixed' in classes:
-        #     color = 'fixed'
         
         tile_data.append({
             'letter': letter,
-            'color': color
+            'color': color,
+            'position': position
         })
+
         count += 1
         if count == 21:
             break
 
-    for data in tile_data:
-        print(data)
-    print(f"Puzzle {i} complete.")
+    return tile_data
 
-    driver.find_element(By.CSS_SELECTOR, "button.button--back.icon-button").click()
+def scroll_to_next_puzzle(driver, puzzle_number):
+    """
+    Pre-Condition: The web page will be in the archive menu and a puzzle to be selected is known and provided
+    Post-Contition: The puzzle board for the current puzzle will have been loaded to the screen.
+    ---------------
+    This function is going to move the menu archive to the next puzzle in the list and open it for interaction with other functions
+    """
+    item = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//*[@data-id='{puzzle_number}']")))
+    driver.execute_script("arguments[0].scrollIntoView();", item)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f"//*[@data-id='{puzzle_number}']"))).click()
 
-    #adds a comments
-    #adds more comments
+if __name__ == '__main__':
+    main()
